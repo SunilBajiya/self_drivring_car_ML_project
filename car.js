95a0b01db8737f762ed36f14f5d5f92474604b10
Sunil Kumar <sunil@algodox.co.in>
2025-02-02 22:48:35 +0530

    new functionality is added in this code

--- car.js
+++ car.js
@@ -1,19 +1,85 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
         this.y = y;
         this.width =width;
         this.height = height;
+
         this.speed =0;
         this.acceleration =0.2;
-        this.maxSpeed =3;
-        this.angle=0;
+        this.maxSpeed =maxSpeed;
         this.friction =0.05;
+        this.angle=0;
+        this.damaged=false;
 
-        this.controls= new Controls();
+        this.useBrain = controlType=="AI";
+
+        if(controlType!="DUMMY"){
+            this.sensor = new Sensor(this);
+            this.brain= new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
+        }
+        this.controls= new Controls(controlType);
     }
-    update(){
-        this.#move();
+    update(roadBorders,traffic){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assertdamage(roadBorders,traffic);
+
+        }
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+            const offsets = this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs= NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
+        }
+    }
+    #assertdamage(roadBorders,traffic){
+        for(let i =0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        for(let i =0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
+                return true;
+            }
+        } 
+        return false;
+
+    }
+    
+
+    #createPolygon(){
+        const points =[];
+        const red = Math.hypot(this.width,this.height)/2;
+        const alpha= Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*red,
+            y:this.y-Math.cos(this.angle-alpha)*red
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*red,
+            y:this.y-Math.cos(this.angle+alpha)*red
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*red,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*red
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*red,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*red
+        });
     }
     #move(){
         if(this.controls.forward){
@@ -50,19 +116,23 @@ class Car{
         this.x-=Math.sin(this.angle)*this.speed;
         this.y-=Math.cos(this.angle)*this.speed;
     }
-    draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
-
+    draw(ctx,color) {
+        if(this.damaged){
+            ctx.fillStyle ="gray";
+        }else{
+            ctx.fillStyle=color;
+        }
         ctx.beginPath();
-    ctx.rect(
-        -this.width/2,
-        -this.height/2,
-        this.width,
-        this.height
-    );
-    ctx.fill();
-    ctx.restore(); 
-}}
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i=1; i<this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
+        ctx.fill();
+
+        if(this.sensor){
+            this.sensor.draw(ctx);
+
+        }
+
+    } 
+}
--- road.js
+++ road.js
@@ -1,31 +1,52 @@
 class Road{
-    constructor(x,width,height){
+    constructor(x,width,laneCount=3){
         this.x=x;
         this.width=width;
-        this.laneCount=this.laneCount;
+        this.laneCount=laneCount;
 
         this.left=x-width/2;
         this.right=x+width/2;
 
 
-        const infinity =1000000;
-        this.top= infinity;
+        const infinity = 1000000;
+        this.top=-infinity;
         this.bottom= infinity;
-    }
+}
+getLaneCenter(laneIndex){
+    const laneWidth = this.width/this.laneCount;
+    return this.left+laneWidth/2+
+    Math.min(laneIndex,this.laneCount-1)*laneWidth;
+} // the minimum lane count right side car
 
-    draw(ctx){
+
+draw(ctx){
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.button);
-        ctx.stroke();
-
-
+        for(let i=0; i<=this.laneCount-1;i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            ctx.setLineDash([20,20]);
+            
+            
             ctx.beginPath();
-            ctx.moveTo(this.right,this.top);
-            ctx.lineTo(this.right,this.button);
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
             ctx.stroke();
-    }
+        }
+        ctx.setLineDash([]);
+        this.borders = [
+            [{x: this.left, y: this.top}, {x: this.left, y: this.bottom}],
+            [{x: this.right, y: this.top}, {x: this.right, y: this.bottom}]
+        ];
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        });
+    }   
 }
